037e58d7b2fe488c2360f0d422751dcc4e7c8f00
litzycm <litzyc13@nycstudents.net>
2026-01-20 14:29:54 +0000

    updates3_6

--- WDLesson3_6Practical/script.js
+++ WDLesson3_6Practical/script.js
@@ -7,6 +7,15 @@ function average(){
 
 
 // Challenge 4:  Create the function for the Slope program.  Refer to the resource link in the HTML for assistance.
-
+function average(){
+    let x1 = parseFloat(document.getElementById Id("x1").value;
+    let x2 = parseFloat(document.getElementById Id("x2").value;
+    let x3 = parseFloat(document.getElementById Id("x3").value;
+    let output = document.getElementById("output average");
 
 // Challenge 6:  Create the function for the BMI program.  Refer to the resource link in the HTML for assistance.
+function average()
+let h1 = parseFloat(document.getElementById Id("h1").value;
+let w1 = parseFloar(document.getElementById Id("w1").value;
+let BMI = weight (kg) / [height (m)]
+output.innerHTML
